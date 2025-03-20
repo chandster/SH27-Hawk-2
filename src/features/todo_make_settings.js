e150af65e88d3a52127eae997f84df32061fb39f
Kieran <2659434w@student.gla.ac.uk>
2025-03-20 12:55:54 +0000

    bugfix/refactored-adjustColourBrightness-to-pass-eslint

--- src/features/todo_make_settings.js
+++ src/features/todo_make_settings.js
@@ -28,18 +28,16 @@ function loadCustomBackground() {
   });
 }
 function adjustColorBrightness(color, percent) {
-  const num = parseInt(color.replace('#', ''), 16);
+  let num = parseInt(color.replace('#', ''), 16);
   const amt = Math.round(2.55 * percent);
-  const R = (num >> 16) + amt;
-  const G = (num >> 8 & 0x00FF) + amt;
-  const B = (num & 0x0000FF) + amt;
-  return `#${(
-    0x1000000
-    + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000
-    + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100
-    + (B < 255 ? B < 1 ? 0 : B : 255)
-  ).toString(16).slice(1)}`;
+
+  let R = Math.min(255, Math.max(0, Math.floor(num / 65536) + amt));
+  let G = Math.min(255, Math.max(0, Math.floor(num / 256) % 256 + amt));
+  let B = Math.min(255, Math.max(0, num % 256 + amt));
+
+  return `#${((1 << 24) | (R << 16) | (G << 8) | B).toString(16).slice(1).toUpperCase()}`;
 }
+
 
 function loadAppearance(usepreference) {
   $('.settings-container').addClass('changing');
@@ -844,7 +842,6 @@ if (window.location.href.startsWith(chrome.runtime.getURL(''))) {
       $('.theme-color-btn').removeClass('active');
       $(`.theme-color-btn[data-color="${color}"]`).addClass('active');
     }
-
 
     const colorPicker = $('#themeColorPicker');
     const defaultColor = '#e65100';
